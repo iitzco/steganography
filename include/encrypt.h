39e6c3a747231df6836b83b6ad69ba1f37e9f926
Federico Bond <federicobond@gmail.com>
2016-05-20 18:34:34 -0300

    Formatting

--- include/encrypt.h
+++ include/encrypt.h
@@ -6,8 +6,10 @@
 typedef A encrypt_cipher;
 typedef M encrypt_mode;
 
-char* encrypt(char* data, encrypt_cipher cipher, encrypt_mode mode, char* password);
+char* encrypt(char* data, encrypt_cipher cipher, encrypt_mode mode,
+              char* password);
 
-char* decrypt(char* data, encrypt_cipher cipher, encrypt_mode mode, char* password);
+char* decrypt(char* data, encrypt_cipher cipher, encrypt_mode mode,
+              char* password);
 
 #endif
