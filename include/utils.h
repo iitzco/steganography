8cc8515d3efcf2a423e16a71eb5d43888219e208
Federico Bond <federicobond@gmail.com>
2016-05-20 09:15:23 -0300

    Remove unused seconds_to_time function

--- include/utils.h
+++ include/utils.h
@@ -1,8 +1,6 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-char* seconds_to_time(float seconds);
-
 long little_to_big_4_bytes(unsigned char buffer4[]);
 
 long little_to_big_2_bytes(unsigned char buffer2[]);
--- src/utils.c
+++ src/utils.c
@@ -3,38 +3,6 @@
 #include <string.h>
 #include <unistd.h>
 
-/**
- * Convert seconds into hh:mm:ss format
- * Params:
- *  seconds - seconds value
- * Returns: hms - formatted string
- **/
-char *seconds_to_time(float raw_seconds) {
-    char *hms;
-    int hours, hours_residue, minutes, seconds, milliseconds;
-    hms = (char *)malloc(100);
-
-    sprintf(hms, "%f", raw_seconds);
-
-    hours = (int)raw_seconds / 3600;
-    hours_residue = (int)raw_seconds % 3600;
-    minutes = hours_residue / 60;
-    seconds = hours_residue % 60;
-    milliseconds = 0;
-
-    // get the decimal part of raw_seconds to get milliseconds
-    char *pos;
-    pos = strchr(hms, '.');
-    int ipos = (int)(pos - hms);
-    char decimalpart[15];
-    memset(decimalpart, ' ', sizeof(decimalpart));
-    strncpy(decimalpart, &hms[ipos + 1], 3);
-    milliseconds = atoi(decimalpart);
-
-    sprintf(hms, "%d:%d:%d.%d", hours, minutes, seconds, milliseconds);
-    return hms;
-}
-
 long little_to_big_4_bytes(unsigned char buffer4[]) {
     return buffer4[0] | (buffer4[1] << 8) | (buffer4[2] << 16) |
            (buffer4[3] << 24);
